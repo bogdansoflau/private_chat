96a3d51815822676a2f1c401d2f8812b7f3e0140
Bogdan Soflau <bogdansoflau01@gmail.com>
2025-12-10 18:52:16 +0100

    site url added as an env

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-// this require .api to enter /api prefix
-export const client = treaty<App>("").api;
+export const client = treaty<App>(process.env.NEXT_PUBLIC_SITE_URL!).api;
